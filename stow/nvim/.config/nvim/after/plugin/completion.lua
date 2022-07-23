4d1fb210e5247bbaa78a1ca15c659ef59e3bc03e
David Davenport <david.davenport88@gmail.com>
2022-07-23 17:03:50 -0400

    Fix nvim-cmp mappings
    Ripped mappings straight from TJ Devries example, so that I could get
    something that actually works.
    
    See: TJ Devries example <https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/after/plugin/completion.lua>

--- stow/nvim/.config/nvim/after/plugin/completion.lua
+++ stow/nvim/.config/nvim/after/plugin/completion.lua
@@ -1,34 +1,94 @@
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 
 cmp.setup {
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = comp.config.window.boredered(),
+    },
+    -- mapping ripped straight from https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/after/plugin/completion.lua
     mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<C-space>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-y>"] = cmp.mapping(
+            cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = true,
+            },
+            { "i", "c" }
+        ),
+
+        ["<C-space>"] = cmp.mapping {
+            i = cmp.mapping.complete(),
+            c = function(
+                _ --[[fallback]]
+            )
+                if cmp.visible() then
+                    if not cmp.confirm { select = true } then
+                        return
+                    end
+                else
+                    cmp.complete()
+                end
+            end,
+        },
+
+        -- ["<tab>"] = false,
+        ["<tab>"] = cmp.config.disable,
+
+        -- ["<tab>"] = cmp.mapping {
+        --   i = cmp.config.disable,
+        --   c = function(fallback)
+        --     fallback()
+        --   end,
+        -- },
+
+        -- Testing
+        ["<c-q>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
-        -- ["<C-space>"] = cmp.mapping.complete(),
-    },
 
+        -- If you want tab completion :'(
+        --  First you have to just promise to read `:help ins-completion`.
+        --
+        -- ["<Tab>"] = function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_next_item()
+        --   else
+        --     fallback()
+        --   end
+        -- end,
+        -- ["<S-Tab>"] = function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_prev_item()
+        --   else
+        --     fallback()
+        --   end
+        -- end,
+    },
     sources = {
         { name = "nvim_lua" },
         { name = "nvim_lsp" },
         { name = "path" },
-        { name = "buffer" },
+        { name = "buffer", keyword_length = 5 },
     },
     formatting = {
-      format = lspkind.cmp_format({
-        mode = "symbol_text",
-        menu = ({
-          buffer = "[Buf]",
-          nvim_lsp = "[LSP]",
-          -- luasnip = "[LuaSnip]",
-          nvim_lua = "[Lua]",
-          latex_symbols = "[LaTeX]",
-        })
-      }),
+        format = lspkind.cmp_format({
+            mode = "text",
+            menu = ({
+                buffer = "[buf]",
+                nvim_lsp = "[LSP]",
+                -- luasnip = "[LuaSnip]",
+                nvim_lua = "[Lua]",
+                path = "[path]",
+                latex_symbols = "[LaTeX]",
+            })
+        }),
     },
 }
